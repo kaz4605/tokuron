102c89586ebf4a3e8b76dabed64bfe5582d940ea
Kazuya Ono <v25y0001@oita-u.ac.jp>
2025-06-09 07:35:16 +0000

    WebAPIの利用と設計

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import { Hono } from 'hono'
 import { zValidator } from '@hono/zod-validator' 
 import { z ,createRoute, OpenAPIHono} from '@hono/zod-openapi'
 import { swaggerUI } from '@hono/swagger-ui'
+import { contextStorage } from 'hono/context-storage'
 
 const app = new OpenAPIHono()
 
@@ -20,5 +21,21 @@ url:'/doc',
 )
 
 app.notFound((c) => c.text('Custom 404 Message',404))
+
+app.get('/github-models',async(c) => {
+  const apiUrl = 'https://models.github.ai/inference/chat/completions'
+  const headers = {
+    'Authorization': `Bearer ${c.env.GITHUB_TOKEN}`,
+    'Content-Type':'application/json'
+  }
+  const body = JSON.stringify({
+    messages:[{role: 'user',content: '生成AIを用いたコーディング手法とは？'}],
+    model:'openai/gpt-4.1'
+  })
+  const response = await fetch(apiUrl, { method: 'POST',headers,body })
+    const data = await response.json()
+    return c.text(data.choices[0].message.content) 
+})
+
 
 export default app
